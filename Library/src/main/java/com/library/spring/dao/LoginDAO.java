dc831f7343119b70a184f95f111e857346a57cca
Krithika Balan <krithika.balan@colorado.edu>
2017-05-01 15:36:07 -0600

    Factory pattern and save user

--- Library/src/main/java/com/library/spring/dao/LoginDAO.java
+++ Library/src/main/java/com/library/spring/dao/LoginDAO.java
@@ -2,5 +2,5 @@ package com.library.spring.dao;
 import com.library.spring.model.*;
 
 public interface LoginDAO{    
-       public boolean checkLogin(String userName, String userPassword);
+       public Users checkLogin(String userName, String userPassword);
 }
--- Library/src/main/java/com/library/spring/dao/LoginDAOImpl.java
+++ Library/src/main/java/com/library/spring/dao/LoginDAOImpl.java
@@ -27,26 +27,31 @@ public class LoginDAOImpl implements LoginDAO{
               return sessionFactory.openSession();
        }
 
-       public boolean checkLogin(String userName, String userPassword){
+       public Users checkLogin(String userName, String userPassword){
 			System.out.println("In Check login");
 			Session session = sessionFactory.openSession();
-			boolean userFound = false;
+			Users u = null;
+			//boolean userFound = false;
 			//Query using Hibernate Query Language
 			String SQL_QUERY =" from Users as o where o.userName=? and o.userPassword=?";
 			Query query = session.createQuery(SQL_QUERY);
 			query.setParameter(0,userName);
 			query.setParameter(1,userPassword);
-			List list = query.list();
+			List<Users> list = query.list();
 
 			if ((list != null) && (list.size() > 0)) {
-				userFound= true;
+				//System.out.println(list.get(0).getUserName());
+				//userFound= true;
+				u = list.get(0);
 			}
 			
-			if (userFound == false)
+			//if (userFound == false)
+			if (u == null)
 			{
 				System.out.println("ValidationFailed");
 			}
 			session.close();
-			return userFound;              
+			//return userFound;
+			return u;
        }
 }
